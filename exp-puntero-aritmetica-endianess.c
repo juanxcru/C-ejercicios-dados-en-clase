2d33b0122106986cd8b3bf6a7005311a4d9d43b2
Juan Cruz Di Nardo <67986196+juanxcru@users.noreply.github.com>
2022-06-27 19:36:32 -0300

    Codigos usados en explicaciones

---
+++ exp-puntero-aritmetica-endianess.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+
+
+    //int entera = 0x4A55414E;
+    int entera = 0x4E41554A;
+
+    char * pc=0;
+    pc=&entera;
+    printf("dir: %p %d %x %c\n",pc,(*pc),(*pc),(*pc));
+
+    pc++;
+    printf("dir: %p %d %x %c\n",pc,(*pc),(*pc),(*pc));
+
+    pc++;
+    printf("dir: %p %d %x %c\n",pc,(*pc),(*pc),(*pc));
+
+    pc++;
+    printf("dir: %p %d %x %c\n",pc,(*pc),(*pc),(*pc));
+
+}
